d9d6fdbeb2f33739ebb615993e2285abcc3edfaf
Hugh Barnes <dev@hubns.com>
2022-02-24 16:26:07 +1300

    Clean up PHP; add stub plugin events and handlers

--- cornercat.php
+++ cornercat.php
@@ -10,54 +10,33 @@ use RocketTheme\Toolbox\Event\Event;
  */
 class CornercatPlugin extends Plugin
 {
-	/**
-	 * @return array
-	 *
-	 * The getSubscribedEvents() gives the core a list of events
-	 *     that the plugin wants to listen to. The key of each
-	 *     array section is the event that the plugin listens to
-	 *     and the value (in the form of an array) contains the
-	 *     callable (or function) as well as the priority. The
-	 *     higher the number the higher the priority.
-	 */
 	public static function getSubscribedEvents()
 	{
 		return [
-			'onPluginsInitialized' => ['onPluginsInitialized', 0]
+			'onPluginsInitialized' => ['onPluginsInitialized', 0],
 		];
 	}
 
-	/**
-	 * Initialize the plugin
-	 */
-	public function onPluginsInitialized()
+	public function onPluginsInitialized(): void
 	{
 		// Don't proceed if we are in the admin plugin
 		if ($this->isAdmin()) {
 			return;
 		}
 
-		// Enable the main event we are interested in
+		// Enable the main events we are interested in
 		$this->enable([
-			'onPageContentRaw' => ['onPageContentRaw', 0]
+			'onAssetsInitialized' => ['addPluginAssets', 0],
+			'onOutputGenerated' => ['addCorner', 0],
 		]);
 	}
 
-	/**
-	 * Do some work for this event, full details of events can be found
-	 * on the learn site: http://learn.getgrav.org/plugins/event-hooks
-	 *
-	 * @param Event $e
-	 */
-	public function onPageContentRaw(Event $e)
-	{
-		// Get a variable from the plugin configuration
-		$text = $this->grav['config']->get('plugins.cornercat.text_var');
-
-		// Get the current raw content
-		$content = $e['page']->getRawContent();
-
-		// Prepend the output with the custom text and set back on the page
-		$e['page']->setRawContent($text . "\n\n" . $content);
+	public function addPluginAssets() {
+		// stub
 	}
+
+	public function addCorner() {
+		// stub
+	}
+
 }
